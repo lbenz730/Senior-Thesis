996b7f9eb61eb3f070878cf4e729f9e13b8095d7
lbenz730 <luke.benz@yale.edu>
2019-04-11 00:58:30 -0400

    Chap2 Graphics

--- chapter_2_graphics.R
+++ chapter_2_graphics.R
@@ -1,19 +1,18 @@
 ### Figure 2.1
 library(ggplot2)
-lines <- read.csv("line_imputation.csv", 
-                  as.is = T)
-ggplot(lines, 
-       aes(x = vegas_line, y = imputed_line)) +
-  geom_point(alpha = 0.35) +
+lines <- read.csv("line_imputation.csv", as.is = T)
+ggplot(lines, aes(x = vegas_line, y = imputed_line)) +
+  geom_vline(xintercept = 0, lty = 2) +
+  geom_hline(yintercept = 0, lty = 2) +
+  geom_point(alpha = 0.2) +
   geom_smooth(method = "lm", color = "orange") +
   theme_bw() +
-  theme(plot.title = element_text(size = 16, 
-                                  hjust = 0.5),
-        plot.subtitle = element_text(size = 12, 
-                                     hjust = 0.5),
+  theme(plot.title = element_text(size = 16, hjust = 0.5),
+        plot.subtitle = element_text(size = 12, hjust = 0.5),
         axis.title = element_text(size = 14)) +
   labs(x = "Vegas Point Spread",
        y = "Imputed Point Spread",
-       title = "Comparison of Point Spreads 
-       (Home Team Perspective)",
-       subtitle = "2018-19 Test Set")
+       title = "Comparison of Point Spreads",
+       subtitle = "Home Team Perspective") +
+  xlim(c(-30, 40)) +
+  ylim(c(-30, 40)) 
--- imputation_evaluation.R
+++ imputation_evaluation.R
@@ -38,6 +38,46 @@ for(i in 1:n) {
   }
 }
 
+x <- read.csv("NCAA_Hoops_Results_2017_Final.csv", as.is = T)
+x <- mutate(x, "pred_score_diff" = predscorediff,
+       "date" = as.Date(paste(year, month, day, sep = "-"))) %>%
+  select(team, opponent, date, pred_score_diff) %>% rbind(
+    select(read.csv("training.csv", as.is = T),
+           team, opponent, date, pred_score_diff)
+    )
+
+files <- unlist(sapply(dir("pbp_data", full.names = T), dir, full.names = T))
+n <- length(files)
+
+for(i in 1:n) {
+  if(i %% 100 == 0) {
+    print(i)
+  }
+  y <- read.csv(files[i], as.is = T)
+  home <- dict$NCAA[dict$ESPN == y$home[1] | dict$ESPN_PBP == y$home[1]]
+  away <- dict$NCAA[dict$ESPN == y$away[1] | dict$ESPN_PBP == y$away[1]]
+  if(length(home) == 0) {
+    home <- NA
+  } 
+  if(length(away) == 0) {
+    away <- NA
+  }
+  date_line <- y$date[1]
+  pred_line <- filter(x, team == home, opponent == away, date == date_line) %>% 
+    pull(pred_score_diff)
+  true_line <- y$home_favored_by[1]
+  
+  if(length(c(pred_line, true_line)) == 2) {
+    if(!is.na(true_line) & !is.na(pred_line)) {
+      if(true_line != pred_line ) {
+        vegas_lines <- c(vegas_lines, true_line)
+        imputed_lines <- c(imputed_lines, pred_line)
+      }
+    }
+  }
+}
+
+
 df <- data.frame("vegas_line" = vegas_lines,
            "imputed_line" = imputed_lines)
 write.csv(df, "line_imputation.csv", row.names = F)
